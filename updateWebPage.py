a8757c217f208f5781f7021411aff017bfc90f76
schallerwf <schallerwf@aol.com>
2017-12-19 22:48:14 -0800

    Adding automated script for adding,commiting, and pushing daily web page

---
+++ updateWebPage.py
@@ -0,0 +1,22 @@
+# This file is an absolutly disgusting hack, 
+# which uses git as a way to update a static web page every day. 
+
+from subprocess import call
+import time
+import os
+from secret import thisDir
+
+pwd = thisDir
+now = time.time()
+today = time.strftime('%Y-%m-%d', time.localtime(now))
+
+print pwd
+os.chdir(pwd)
+
+# Run update script to generate HTMl
+call(["python", "scripts/dailyStats.py", "data/"+today+"_surfData.csv", "txt/validLocationKeysWithSurfData", "html/index.html_template", "how-is-the-surf-today.html"])
+
+# Add, commit, and push index file.
+call(["git", "add", "index.html"])
+call(["git", "commit", "-m" "Automated commit updating index.html with todays surf report."])
+call(["./push.exp"])
